56990a500cc39fea0c06d6378ed5e39effa0a234
Keith Niño P. Lumelay <120693819+lumkl@users.noreply.github.com>
2023-01-20 11:32:11 +0800

    Create Character.java

---
+++ Exercise 3/Character.java
@@ -0,0 +1,10 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+	public void attack() {
+        System.out.println("I am attacking");
+	}
+   
+}
