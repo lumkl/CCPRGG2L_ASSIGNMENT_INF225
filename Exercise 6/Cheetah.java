7fdb761e285a048b27c4eff79da2048f60dc0814
Keith Niño P. Lumelay <120693819+lumkl@users.noreply.github.com>
2023-03-17 05:33:39 +0800

    Create Cheetah.java

---
+++ Exercise 6/Cheetah.java
@@ -0,0 +1,10 @@
+public class Cheetah implements Animal {
+
+    public void walk() {
+        System.out.println("Cheetah is Walking");
+    }
+    
+    public void run(){
+        System.out.println("Cheetah is Running");
+    }
+}
